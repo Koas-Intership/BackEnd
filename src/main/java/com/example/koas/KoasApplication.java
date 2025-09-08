0abbe55eea2cba49e6b827ca6da721b9882c0ade
kimjunghwannn <aa2002dcks@naver.com>
2025-09-08 15:37:00 +0900

    first commit

---
+++ src/main/java/com/example/koas/KoasApplication.java
@@ -0,0 +1,13 @@
+package com.example.koas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoasApplication.class, args);
+    }
+
+}
